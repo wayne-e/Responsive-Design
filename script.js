27a3c5a22e0d992c9a56d45b6c1613b2503b28b8
Wayne <luiguiloarca@gmail.com>
2020-10-20 13:43:15 -0600

    Navbar funcional

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const sections = [...document.querySelectorAll('section')];
 const getLinkById = id => document.querySelector(`a[href='#${id}']`);
+const barsIcon = document.getElementById("bars-icon")
+const mainNavbar = document.getElementById("navbar");
+const navLinks = document.getElementsByClassName("nav-link");
 
 const inView = section => {
     let top = section.offsetTop;
@@ -8,20 +11,30 @@ const inView = section => {
         section = section.offsetParent;
         top += section.offsetTop;
     }
-    return(
-        top<(window.pageYOffset+window.innerHeight)&&
-        (top+height)>window.pageYOffset
+    return (
+        top < (window.pageYOffset + window.innerHeight) &&
+        (top + height) > window.pageYOffset
     );
 };
-window.onscroll = ()=>{
-    let next =false;
+window.onscroll = () => {
+    let next = false;
     sections.forEach(section => {
         const a = getLinkById(section.id);
-        if(inView(section)&& !next){
+        if (inView(section) && !next) {
             a.classList.add('main-navbar--current');
-            next=true;
-        }else{
+            next = true;
+        } else {
             a.classList.remove('main-navbar--current');
         }
     });
 };
+
+barsIcon.addEventListener("click", function () {
+    mainNavbar.classList.toggle("display-bar");
+});
+
+for(i=0;i<navLinks.length;i++){
+    navLinks[i].addEventListener("click", function(){
+        mainNavbar.classList.remove("display-bar");
+    });
+}
