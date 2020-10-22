188b7c45338c4f5a1902e55ce49064bb564c6cd5
Wayne <luiguiloarca@gmail.com>
2020-10-22 16:03:08 -0600

    Cambios en modals

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const barsIcon = document.getElementById("bars-icon")
 const mainNavbar = document.getElementById("navbar");
 const navLinks = document.getElementsByClassName("nav-link");
 const productDiv = document.getElementById("product-div");
-const productElement = document.getElementsByClassName("product-element");
+const productElement = document.getElementsByClassName("product-item");
+const backProductButton = document.getElementById("back-product-btn");
+const foodCatalog = document.getElementById("food-catalog");
+const drinkCatalog = document.getElementById("drink-catalog");
+const dessertCatalog = document.getElementById("dessert-catalog");
+const allProductsCatalog = document.getElementById("all-products-catalog");
+const foodDiv = document.getElementById("food-div");
+const drinkDiv = document.getElementById("drink-div");
+const dessertDiv = document.getElementById("dessert-div");
+const closeCatalogButton = document.getElementById("close-catalog-button");
+const addtoCar = document.getElementById("add-to-cart");
+const successDiv = document.getElementById("success-div");
+const closeCartButton = document.getElementById("close-cart-button");
+const shoppingCartDiv = document.getElementById("shopping-cart-div");
+const shoppingCartIcon = document.getElementById("shopping-cart");
 
 const inView = section => {
     let top = section.offsetTop;
@@ -35,14 +49,50 @@ barsIcon.addEventListener("click", function () {
     mainNavbar.classList.toggle("display-bar");
 });
 
-for(i=0;i<navLinks.length;i++){
-    navLinks[i].addEventListener("click", function(){
+backProductButton.addEventListener("click", function () {
+    productDiv.classList.add("display-none");
+});
+
+foodCatalog.addEventListener("click", function () {
+    allProductsCatalog.classList.remove("display-none");
+    foodDiv.classList.remove("display-none");
+});
+drinkCatalog.addEventListener("click", function () {
+    allProductsCatalog.classList.remove("display-none");
+    drinkDiv.classList.remove("display-none");
+});
+dessertCatalog.addEventListener("click", function () {
+    allProductsCatalog.classList.remove("display-none");
+    dessertDiv.classList.remove("display-none");
+});
+closeCatalogButton.addEventListener("click", function () {
+    foodDiv.classList.add("display-none");
+    drinkDiv.classList.add("display-none");
+    dessertDiv.classList.add("display-none");
+    allProductsCatalog.classList.add("display-none");
+});
+addtoCar.addEventListener("click", function () {
+    successDiv.classList.remove("display-none");
+    setTimeout(function () {
+        successDiv.classList.add("display-none");
+    }, 1000);
+});
+shoppingCartIcon.addEventListener("click", function () {
+    shoppingCartDiv.classList.remove("display-none");
+});
+closeCartButton.addEventListener("click", function () {
+    shoppingCartDiv.classList.add("display-none");
+});
+
+
+for (i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click", function () {
         mainNavbar.classList.remove("display-bar");
     });
 }
 
-for(i=0;i<productElement.length;i++){
-    productElement[i].addEventListener("click", function(){
+for (i = 0; i < productElement.length; i++) {
+    productElement[i].addEventListener("click", function () {
         productDiv.classList.remove("display-none");
     });
 }
